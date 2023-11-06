2c53a05713fda750b03e0206d1ade35c5f4ec40f
Adam Slay <aslay@mesonet.org>
2023-11-06 15:47:23 -0600

    add build.bat file to build on windows machines

---
+++ build.bat
@@ -0,0 +1,18 @@
+@echo off
+
+REM Check if vcpkg is installed
+if not exist "vcpkg" (
+    REM Clone vcpkg repository
+    git clone https://github.com/microsoft/vcpkg.git
+    REM Bootstrap vcpkg
+    .\vcpkg\bootstrap-vcpkg.bat
+)
+
+REM Install dependencies
+.\vcpkg\vcpkg install sdl2
+.\vcpkg\vcpkg install sdl2-image
+.\vcpkg\vcpkg install nlohmann-json
+
+REM Configure and build project
+cmake -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake -B build
+cmake --build build
