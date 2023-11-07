8e809b1181cef906f226d6053c2cb975e2596b39
Adam Slay <aslay@mesonet.org>
2023-11-07 15:43:53 -0600

    refactor Level to initialize its own colliders vector<SDL_rect>

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,6 @@ int main( int argc, char* argv[] ) {
     Camera camera;
     Player player(renderer, "Player");
     Level level(renderer);
-
     run_game_loop(player, level, camera, renderer);
     close(renderer, window);
 
