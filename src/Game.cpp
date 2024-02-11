841a6c53995c8f31b01cb4deb74f922bc2c1e81d
Adam Slay <aslay@mesonet.org>
2024-02-11 12:49:26 -0600

    Game class scaffolding

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,56 +1,51 @@
-#include <iostream>
-
-#include "Game.h"
-
-#include "utils.h"
-
-
-Game::Game(Player& player, Level& level, Camera& camera, SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font)
-        : player(player), level(level), camera(camera), window(window), renderer(renderer), font(font), quit(false) {
-        }
-
-Game::~Game() {
-    close();
-}
-
-void Game::run() {
-    while (!quit) {
-        start_menu();
-        game_loop();
-    }
-}
-
-void game_loop() {
-
-
-}
-
-
-void run_loop(){
-
-
-}
-
-
-void start_menu(){
-
-
-}
-
-
-void handle_keyboard_events(SDL_Event& e){
-
-
-}
-
-
-void render_score(const std::string& text, SDL_Color color, int x, int y){
-
-
-}
-
-bool check_collision(SDL_Rect a, SDL_Rect b){
-
-   return false;
-}
-
+//#include <iostream>
+//
+//#include "Game.h"
+//
+//#include "utils.h"
+//
+//
+//Game::Game(Player& player, Level& level, Camera& camera, SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font)
+//        : player(player), level(level), camera(camera), window(window), renderer(renderer), font(font), quit(false) {
+//        }
+//
+//Game::~Game() {
+//}
+//
+//void Game::run() {
+//}
+//
+//void game_loop() {
+//
+//
+//}
+//
+//
+//void run_loop(){
+//
+//
+//}
+//
+//
+//void start_menu(){
+//
+//
+//}
+//
+//
+//void handle_keyboard_events(SDL_Event& e){
+//
+//
+//}
+//
+//
+//void render_score(const std::string& text, SDL_Color color, int x, int y){
+//
+//
+//}
+//
+//bool check_collision(SDL_Rect a, SDL_Rect b){
+//
+//   return false;
+//}
+//
